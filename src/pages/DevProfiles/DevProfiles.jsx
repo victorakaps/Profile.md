b455c3a34f8dc50fb2e8a675b347e6f613cfc03a
neeraj15022001 <gneeraj32595@gmai.com>
2021-10-05 16:37:35 +0530

    added api stuff

--- src/pages/DevProfiles/DevProfiles.jsx
+++ src/pages/DevProfiles/DevProfiles.jsx
@@ -1,9 +1,23 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { DarkModeContext, ProfileCard, Navbar } from './imports';
 import './DevProfiles.css';
+import { fetchData } from './imports';
 
 function DevProfiles() {
     const [toggle] = useContext(DarkModeContext);
+    const [data, setdata] = useState([]);
+    useEffect(() => {
+        let mounted = true;
+        fetchData().then((cardData) => {
+            if (mounted) {
+                setdata(cardData);
+            }
+        });
+        return () => {
+            mounted = false;
+        };
+    }, []);
+    // {console.log("printing data in DEvProfiles", data)}
     return (
         <section
             className={`overflow-auto dev-profiles-container ${
@@ -11,67 +25,17 @@ function DevProfiles() {
             }`}
         >
             <Navbar title="dev profiles" />
-            <section className="overflow-auto" style={{height: "calc(100% - 90px)"}}>
+            <section className="overflow-auto" style={{ height: 'calc(100% - 90px)' }}>
                 <div className="p-4 row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 gy-4 overflow-auto align-items-center justify-content-center h-100">
-                    <div className="col">
-                        <ProfileCard
-                            imageURL="https://images.pexels.com/photos/965345/pexels-photo-965345.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                            title="Title"
-                            content="Cillum qui do consequat dolor enim elit eu tempor esse quis ut."
-                        />
-                    </div>
-
-                    <div className="col">
-                        <ProfileCard
-                            imageURL="https://images.pexels.com/photos/965345/pexels-photo-965345.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                            title="Title"
-                            content="Cillum qui do consequat dolor enim elit eu tempor esse quis ut."
-                        />
-                    </div>
-
-                    <div className="col">
-                        <ProfileCard
-                            imageURL="https://images.pexels.com/photos/965345/pexels-photo-965345.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                            title="Title"
-                            content="Cillum qui do consequat dolor enim elit eu tempor esse quis ut."
-                        />
-                    </div>
-
-                    <div className="col">
-                        <ProfileCard
-                            imageURL="https://images.pexels.com/photos/965345/pexels-photo-965345.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                            title="Title"
-                            content="Cillum qui do consequat dolor enim elit eu tempor esse quis ut."
-                        />
-                    </div>
-                    <div className="col">
-                        <ProfileCard
-                            imageURL="https://images.pexels.com/photos/965345/pexels-photo-965345.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                            title="Title"
-                            content="Cillum qui do consequat dolor enim elit eu tempor esse quis ut."
-                        />
-                    </div>
-                    <div className="col">
-                        <ProfileCard
-                            imageURL="https://images.pexels.com/photos/965345/pexels-photo-965345.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                            title="Title"
-                            content="Cillum qui do consequat dolor enim elit eu tempor esse quis ut."
-                        />
-                    </div>
-                    <div className="col">
-                        <ProfileCard
-                            imageURL="https://images.pexels.com/photos/965345/pexels-photo-965345.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                            title="Title"
-                            content="Cillum qui do consequat dolor enim elit eu tempor esse quis ut."
-                        />
-                    </div>
-                    <div className="col">
-                        <ProfileCard
-                            imageURL="https://images.pexels.com/photos/965345/pexels-photo-965345.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                            title="Title"
-                            content="Cillum qui do consequat dolor enim elit eu tempor esse quis ut."
-                        />
-                    </div>
+                    {data.map((item, index) => (
+                        <div className="col" key={index}>
+                            <ProfileCard
+                                imageURL="https://images.pexels.com/photos/965345/pexels-photo-965345.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                                title={item.username}
+                                content="Cillum qui do consequat dolor enim elit eu tempor esse quis ut."
+                            />
+                        </div>
+                    ))}
                 </div>
             </section>
         </section>
--- src/pages/DevProfiles/imports.js
+++ src/pages/DevProfiles/imports.js
@@ -1,4 +1,5 @@
 import ProfileCard from '../../components/ProfileCard/ProfileCard';
 import { DarkModeContext } from '../../context/DarkModeContext/exports';
 import { Navbar } from '../Contributors/imports';
-export { DarkModeContext , ProfileCard, Navbar};
+import {fetchData} from "../../services/api"
+export { DarkModeContext , ProfileCard, Navbar, fetchData};
